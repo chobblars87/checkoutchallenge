481aadec88d286d9c8f11a0aa36c169af6b293d7
Paul Barns <p.barns@gmail.com>
2020-08-19 17:48:00 +0800

    Updated based on the google doc latest ver

--- index.php
+++ index.php
@@ -36,91 +36,120 @@
 	$body = '<div id="main">
 				<div class="container">
 					<div class="intro">
-						Thanks for taking your first interview with Adyen! <br/><br/>
-						For the next stage, we\'d love for you to complete the below tech challenges. If you could complete this within the next 10 days, it would be much appreciated! <br/>
-						Based on your submission, we’ll then schedule the next round of interviews! <br/><br/>
-						There will be two parts to this technical test...<br/><br/>
-
+						Thanks for taking your time today! Please see the two follow up challenges below as part of your next step. <br/><br/>
 					</div>
-					<div  class="card">
+					<div class="card">
 						<div class="challenge card-body">
-							<h5>Challenge 1: The Adyen (not so) Tech Test </h5><br/>
+							<h5>Challenge 1: Checkout, Checkout, Checkout</h5><br />
 
-							Please follow the link below to access a quick quiz and fill out your answers. This should not take any longer than 10 minutes, but please take as much time as you need.<br/><br/>
+							You will be using Adyen\’s Checkout Drop-in web solution to integrate a typical eCommerce shopper checkout flow. Follow the instructions below and don\'t hesitate to reach out to us if you need any help!<br /><br />
 
-							<a href="https://www.surveymonkey.com/r/87YYFFZ">https://www.surveymonkey.com/r/87YYFFZ</a>
-						</div>
-						<div class="challenge card-body">
-							<hr class="mb-4">
+							Using the programming language of your choice, integrate the following (including backend server):<br />
+							<a href="https://docs.adyen.com/checkout/drop-in-web">https://docs.adyen.com/checkout/drop-in-web</a><br /><br />
 
-								<h5>Challenge 2: Integrate to Adyen\'s Platform</h5><br/>
-								<div class="intro">
-									One of the integration options with Adyen is to integrate with our Checkout API (back-end) & Drop-in Components (front-end). Using the programming language of your choice integrate the following (including back-end server):<br/><br/>
+							At the minimum, your integration needs to meet the conditions below:<br />
 
-									<b>Frontend</b><br/><br/>
-
-									With the Drop-in solution handing the front-end, you would be able display the required fields for each payment method without needing to know the actual flow of each payment method.
-
-									<div class="sc-notice info"><div>
-									<p>Hint: The .env.default file needs to be saved in the systems format.</p>
-									</div></div>
-									<br/>
-									<b>Backend</b><br/><br/>
-
-									Make the necessary API calls like <code>/paymentMethods</code>, <code>/payments</code> and <code>/payment/details</code> to Adyen from your back-end server to complete the payment. You would also be able to try the responses with our API explorer just to get familiarize with the formats of the Checkout API - <a href="https://docs.adyen.com/api-explorer/#/PaymentSetupAndVerificationService/v49/payments">https://docs.adyen.com/api-explorer/#/PaymentSetupAndVerificationService/v49/payments</a>. <br/><br/>
-
-									<b>3D 2 Secure Flow</b><br/><br/>
-
-									In payments, we struggle to maintain the balance between Fraud and Auth Rate. As much as we want to get all the transactions through seamlessly, we do not want to let the fraudsters into our radar. As such we balance this by introducing the new standard for 3D secure from the card schemes. You can refer to more information here - <a href="https://docs.adyen.com/checkout/3d-secure">https://docs.adyen.com/checkout/3d-secure<a/><br/><br/>
-
-									To help you with creating the 3D2, you can refer to the web (or mobile should you wish) payment - <a href="https://docs.adyen.com/checkout/3d-secure/native-3ds2/api-integration#submit-a-payment-request">https://docs.adyen.com/checkout/3d-secure/native-3ds2/api-integration#submit-a-payment-request</a>, if you are using Drop-in, you would need to handle <code>onAdditionalDetails</code> (<a href="https://docs.adyen.com/checkout/drop-in-web#step-4-additional-front-end">https://docs.adyen.com/checkout/drop-in-web#step-4-additional-front-end</a>)<br/><br/>
-
-									<hr class="mb-4">
-
-									<div class="sc-notice info">
-										Please feel free to run this locally on your machine using any of the following ports: <br />
-										<ul>
-											<li>3000</li>
-											<li>5000</li>
-											<li>8000</li>
-											<li>8080</li>
+							<div class="sc-notice info">
+								<ol>
+									<li>Accept card payments (Visa and MasterCard)</li>
+									<li>Accept at least 1 local payment method - for example:
+									 	<ul>
+											<li>Alipay (for CN/SG)</li>
+											<li>POLi (for AU/NZ)</li>
+											<li>iDeal (NL)</li>
 										</ul>
-										Using any of these ports will allow you to successfully connect to Adyen using the following clientKey:<br/>
-										<pre><code>test_GWXWP766DVDVHP3NUESVCEBV5AKZCOGJ</code></pre>
+									</li>
+									<li>Perform 3DS2 on all card payments - <a href="https://docs.adyen.com/checkout/3d-secure/redirect-3ds2-3ds1/web-drop-in">https://docs.adyen.com/checkout/3d-secure/redirect-3ds2-3ds1/web-drop-in</a></li>
+								</ol>
+							</div>
+							<br />
+							Once you have completed the integration, please be prepared to walk us through it (including any challenges you had) during your next interview. Additionally, please share the following:<br />
+							<br />
+							<ul>
+								<li>2 pspReferences (unique payment reference) for two example payments - one card payment and one local payment method.</li>
+								<li>Example requests / responses for the above-mentioned pspReferences for all API calls (merchantAccount: AdyenRecruitmentCOM)</li>
+								<li>Your entire project to us via a ZIP file/Github <strong><u>before</u></strong> the interview so that we can also look at it.</li>
+							</ul>
+							When making your payment request, make sure that the value for your reference field is set to: ' . (isset($_GET['firstname']) ? $_GET['firstname'] : '{yourFirstName}') . '_checkoutChallenge.
+							<br /><br />
+							With regards to the overall UI, feel free to design it in any way you please. Also, the above three conditions are the baseline integration, but feel free to add on more functionality to your checkout flow.
+							<br /><br />
+							Lastly, as there are example integrations online, <u><strong>no pre-built libraries or example code</strong> may be used in your solution</u>.
+							<br />
+
+								Here are some examples of what we are referring to:
+								<div class="multi-notice">
+									<div class="sc-notice info rorw ok">
+										Ok to use:<br />
+										<div class="fillspace">
+											<ul>
+												<li>PHP: cURL library for HTTP POST requests</li>
+												<li>Python: requests library</li>
+												<li>Node: express/axios</li>
+												<li>C#: HttpClient</li>
+											</ul>
+										</div>
 									</div>
-									During your next interview, we will ask you to walk us through your code and your demo implementation. Additionally, please share the following:<br/><br/>
-
-									<ul>
-		  							<li>Example requests / responses from your integration for all API calls</li>
-		  							<li>2 PSP references (unique payment reference) for two example payments - one using Visa/Mastercard with 3DS2, and one alternative local payment of your choice (Bank transfer / Vouchers / etc)</li>
-									</ul>
-
-										<div class="sc-notice info"><div>
-										<p>Hint: You should be able to see alternative payment methods if you use the following combination of currency and country code in the paymentMethods request:
-											<pre><code>{ "countryCode":"DE", "amount":{ "currency":"EUR", "value":1000 } }</code></pre></p>
-										</div></div>
-
-										<div class="sc-notice info"><div>
-										<p>Useful links:
+									<div class="sc-notice info rorw not">
+										Not ok to use: any pre-built library that interacts directly with Adyen over API. For example:
 										<ul>
-											<li>Adyen Drop-in Integration: <a href="https://docs.adyen.com/checkout/drop-in-web">https://docs.adyen.com/checkout/drop-in-web</a></li>
-											<li>Adyen Git Libraries: <a href="https://github.com/Adyen">https://github.com/Adyen</a></li>
-											<li>Adyen Test Cards: <a href="https://docs.adyen.com/developers/test-cards/test-card-numbers">https://docs.adyen.com/developers/test-cards/test-card-numbers</a></li>
+											<li><a href = "https://github.com/Adyen/adyen-python-api-library">https://github.com/Adyen/adyen-python-api-library</a></li>
+											<li><a href = "https://github.com/Adyen/adyen-java-api-library">https://github.com/Adyen/adyen-java-api-library</a></li>
+											<li><a href = "https://github.com/Adyen/adyen-php-api-library">https://github.com/Adyen/adyen-php-api-library</a></li>
+											<li><a href = "https://github.com/Adyen/adyen-dotnet-api-library">https://github.com/Adyen/adyen-dotnet-api-library</a></li>
+											<li><a href = "https://github.com/Adyen/adyen-node-api-library">https://github.com/Adyen/adyen-node-api-library</a></li>
 										</ul>
-										</p>
 									</div>
-								</div>';
+								</div>
+
+							<br />
+							<strong>Helpful Links</strong>
+							<div class="sc-notice info">
+								<ul>
+									<li>
+										Drop-in Documentation
+										<ul>
+											<li><a href="https://docs.adyen.com/checkout/drop-in-web">https://docs.adyen.com/checkout/drop-in-web</a></li>
+											<li><a href="https://docs.adyen.com/checkout/3d-secure/redirect-3ds2-3ds1/web-drop-in">https://docs.adyen.com/checkout/3d-secure/redirect-3ds2-3ds1/web-drop-in</a></li>
+										</ul>
+									</li>
+									<li>Test credentials: <a href="https://docs.adyen.com/developers/test-cards/test-card-numbers">https://docs.adyen.com/developers/test-cards/test-card-numbers</a></li>
+									<li>Code examples: <a href="https://github.com/Adyen/">https://github.com/Adyen/</a></li>
+								</ul>
+							</div>
+							<br />
+							<strong>Authentication & Credentials</strong>
+							<div class="sc-notice info">
+								<ul>
+									<li>merchantAccount: <code>AdyenRecruitmentCOM</code></li>
+									<li>API Key (x-api-key): <code>AQE1hmfxKo3NaxZDw0m/n3Q5qf3Ve55dHZxYTFdTxWq+l3JOk8J4BO7yyZBJ4o0JviXqoc8j9sYQwV1bDb7kfNy1WIxIIkxgBw==-q7XjkkN/Cud0WELZF+AzXpp/PuCB8+XmcdgqHYUWzTA=-Kk9N4dG837tIyjZF</code></li>
+									<li>Client Key (clientKey): <code>test_GWXWP766DVDVHP3NUESVCEBV5AKZCOGJ</code></li>
+								</ul>
+							</div>
+							<div class="sc-notice info">
+							The permitted domains for the above <strong>clientKey</strong> are: <br />
+								<ul>
+									<li>http://localhost:3000</li>
+									<li>http://localhost:5000</li>
+									<li>http://localhost:8000</li>
+									<li>http://localhost:8080</li>
+									<li>http://127.0.0.1:5000</li>
+								</ul>
+								Please ensure that your website is running on one of the above permitted domains; if not you will be unable to load your credit card fields.<br /><em style="font-size:14px">If you do want to run your server on another domain, feel free to reach out to me and we will add it for you.</em>
+							</div>
+							<br />
+							If you are in the midst of testing credit card payments, and are getting a 422: Unable to decrypt data error, that\'s an issue with the domain where you are hosting your front-end website or the clientKey. Nonetheless, feel free to reach out to us if you still cannot resolve the error.
+							<br /><br />
+							You don’t need to publicly host your website (however, please feel free to do so). We will schedule a Zoom call for you to run through your solution with us.';
+
 
 					$footer = '<div id="footer">
 									<hr class="mb-4">
 
-									<b>Authentication</b><br/><br/>
-									In order to interface with the Adyen API\'s, you would normally be required to generate a new API Key for your "WS User". However for ease, please use the following key for all Adyen API interactions:<br/><br/>
-									<div style="margin-left: 40px"><code>AQE1hmfxKo3NaxZDw0m/n3Q5qf3Ve55dHZxYTFdTxWq+l3JOk8J4BO7yyZBJ4o0JviXqoc8j9sYQwV1bDb7kfNy1WIxIIkxgBw==-q7XjkkN/Cud0WELZF+AzXpp/PuCB8+XmcdgqHYUWzTA=-Kk9N4dG837tIyjZF</code></div>
-									<br/>
+									<b>Adyen\'s Dashboard (Web Portal)</b><br/><br/>
 									<div id="caReg">
 										<div class="instructions">
-											The next step is to create a user for the Adyen Dashboard. This is where you can access our portal in to the platform, and see your payments coming in once you\'ve completed the first part of the integration and can help diagnose any problems you may be facing during your integration.
+											The final piece of the puzzle that you will need is to a user for the Adyen Dashboard; a portal from where you can access data and insights in to the platform, and see your payments coming in once you\'ve completed the first part of the integration to help diagnose any problems you may be facing.
 										</div>
 										<form id="reqUser">
 											<br/>
@@ -140,8 +169,8 @@
 									}
 
 									$footer .= 'in order to be sent an invite link to the Adyen Dashboard before clicking submit:
-											</div>
-											<br/>
+											</div><br />
+
 											<div class="row">
 								            	<div class="col-md-6 mb-3">
 									                <label for="firstName">First name</label>
@@ -164,12 +193,21 @@
 								</div>';
 
 				$endbody = '</div>
-
 							<!-- replacement caUser registration div -->
 							<div id="userCreated" style="display: none;">
 
 							</div>
+							<hr class="mb-4 nobot">
+							<div class="challenge card-body">
+
+								<h5>Challenge 2: The Adyen (not so) Tech Test </h5><br/>
+								In addition, please follow the link below to access a quick quiz and fill out your answers. This should not take any longer than 30 minutes, but please take as much time as you need!<br/><br/>
+								<a href="https://www.surveymonkey.com/r/87YYFFZ">https://www.surveymonkey.com/r/87YYFFZ</a>
+								<div class="bottom-spacer"></div>
+							</div>
 						</div>
+						<br />Please reach out if you have any questions. Best of luck and I hope you enjoy the challenge!<br />
+						<div class="bottom-spacer"></div>
 					</div>
 				</div>
 			</div>';
@@ -200,13 +238,13 @@
 
 <html>
 	<head>
-    	<link type="image/ico" rel="shortcut icon" href="images/favicon.ico">
+    <link type="image/ico" rel="shortcut icon" href="images/favicon.ico">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 		<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 		<link rel="stylesheet" href="style.css">
-	  	<script src="addons/jstimezonedetect/dist/jstz.min.js"></script>
+	 	<script src="addons/jstimezonedetect/dist/jstz.min.js"></script>
 	</head>
 	<body>
 		<div id="header">
@@ -222,7 +260,7 @@
 
 		<script>
 
-			var emailhtml = '<?php echo base64_encode(htmlentities(/*$styles . */$body . $endbody)); ?>';
+			var emailhtml = '<?php echo base64_encode(htmlentities($body . $endbody)); ?>';
 
 			$(function() {
 
